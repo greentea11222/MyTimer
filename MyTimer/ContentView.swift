cacab530cca69919b48267e855cdcc1c1186a10d
greentea11222 <oregreen1122@gmail.com>
2026-01-29 23:12:41 +0900

    秒数設定ボタンを追加、色設定とアイコンを追加

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -9,15 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack {
+            VStack {
+                Text("タイマー画面")
+            }
+            //ナビゲーションにボタンを追加
+            .toolbar {
+                //ナビゲーションバーの右にボタンを追加
+                ToolbarItem(placement: .topBarTrailing) {
+                    //ナビゲーション遷移
+                    NavigationLink {
+                        //遷移先（設定画面）
+                        SettingView()
+                    } label : {
+                        //テキストを表示
+                        Text("秒数設定")
+                    } //NavigationLinkここまで
+                } //ToolbarItemここまで
+            } // .toolbarここまで
+        } //NavigationStackここまで
+    } //bodyここまで
+} //ContentViewここまで
 
 #Preview {
     ContentView()
---
+++ MyTimer/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  MyTimer
+//
+//  Created by USER on 2026/01/29.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
